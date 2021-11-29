d8b1a07f2668e233fe51b3bae4db92bc4eb4133e
hsadiq32 <20135112s@gmail.com>
2021-11-29 17:58:34 +0000

    Moved repo

---
+++ Code/Razor Pages/blockbusters/Models/OrderItem.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace blockbusters.Models
+{
+    public class OrderItem
+    {
+        [Key]
+        public int OrderItemID { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal SalePrice { get; set; }
+
+        // NAVIGATION PROPERTIES
+        public virtual Order Order { get; set; }
+
+        public virtual Movie Movie { get; set; }
+    }
+}
---
+++ Code/Razor Pages/blockbusters/Models/PersonGenre.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace blockbusters.Models
+{
+    public class PersonGenre
+    {
+        [Key]
+        public int PersonGenreID { get; set; }
+        // NAVIGATION PROPERTIES
+        public Genre Genre { get; set; }
+
+
+        public virtual Person Person { get; set; }
+    }
+}
